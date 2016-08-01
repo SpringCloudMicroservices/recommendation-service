3899d22b10c7fffb7c19b2ffae75bd61d7848052
Tri Bui Pham Minh <tri.bui@pyramid-consulting.com>
2016-08-01 16:30:06 +0700

    Enable DiscoveryClient

--- src/main/java/com/pycogroup/examples/recommendation/Application.java
+++ src/main/java/com/pycogroup/examples/recommendation/Application.java
@@ -2,18 +2,22 @@ package com.pycogroup.examples.recommendation;
 
 import com.netflix.appinfo.InstanceInfo;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaStatusChangedEvent;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
 /**
  * Created by tri.bui on 7/20/16.
  */
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableEurekaClient
 @EnableFeignClients
